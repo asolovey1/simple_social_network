f116e133eda01ace41214efeb5c2793923735eea
Aviva Solovey <asolovey@terpmail.umd.edu>
2025-04-06 13:57:52 -0400

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,14 @@
+"""A simple social network for tracking connections between poeple."""
+
+
+class Person: 
+    """A person in the social network.
+    
+    Attributes:
+        name (str): the person's name
+        connetions (set of Person): other people in the social network who know
+            this person.
+    """
+    
+    
+    
