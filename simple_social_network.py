64480fe4f895010b6b9a31aa849ec49d6c9262de
Aviva Solovey <asolovey@terpmail.umd.edu>
2025-04-06 14:15:55 -0400

    Created skeleton of connet() method.

--- simple_social_network.py
+++ simple_social_network.py
@@ -11,4 +11,16 @@ class Person:
     """
     
     
+    def __init__(self, name):
+        """Initialize a new Person object."""
+        self.name = name
+        self.connections = set()
+        
+        
+def connect(self, person2):
+    """Connect with person2.
+    
+    Args:
+        person2 (Person): the other person to connect to.
+    """
     
